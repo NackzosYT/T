9d20c882e5a9b9b91ad1de7afe50b5d37217a5dc
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-11-07 21:49:12 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,140 +39,94 @@ client.on('message', message => {
 });
 
 
-
-
-
-
-
-
-
-
-client.on('guildMemberAdd',async member => {
-  const Canvas = require('canvas');
-  const jimp = require('jimp');
-  const w = ['./welcome22.png'];
-        let Image = Canvas.Image,
-            canvas = new Canvas(800, 300),
-            ctx = canvas.getContext('2d');
-        ctx.patternQuality = 'bilinear';
-        ctx.filter = 'bilinear';
-        ctx.antialias = 'subpixel';
-        ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-        ctx.shadowOffsetY = 2;
-        ctx.shadowBlur = 2;
-        ctx.stroke();
-        ctx.beginPath();
+// !$say
+ client.on('message', message => {
+          var prefix = "S";
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+   let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+   let args = message.content.split(" ").slice(1);
+ // !$say
+  if (command === "say") {
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
+  
  
-        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-            if (err) return console.log(err);
-            let BG = Canvas.Image;
-            let ground = new Image;
-            ground.src = Background;
-            ctx.drawImage(ground, 0, 0, 800, 300);
- 
+ if (command == "emb") {
+    let say = new Discord.RichEmbed()
+  .setThumbnail(message.author.avatarURL)  
+  .setAuthor(message.author.username)
+    .setDescription(args.join("  "))
+    .setColor(0x00AE86)
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+ });
+ client.on('message', message => {
+          var prefix = "S";
+    if(message.content.startsWith(prefix + 'avatar')) {
+        var mentionned = message.mentions.users.first();
+          var getvalueof;
+          var bot;
+          if(mentionned) {
+            getvalueof = mentionned;
+          } else {
+            getvalueof = message.author;
+          }
+          let avatar = new Discord.RichEmbed()
+          .setTitle(`${getvalueof.username}\'s Avatar`)
+          .setImage(`${getvalueof.avatarURL}`);
+          message.channel.sendEmbed(avatar);
+      }
+
+
+
+
+client.on("message", message => {
+          var prefix = "S";
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
 })
- 
-                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-                jimp.read(url, (err, ava) => {
-                    if (err) return console.log(err);
-                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                 if (err) return console.log(err);
- 
-          ctx.font = '36px Arial';
-          ctx.fontSize = '72px';
-          ctx.fillStyle = "#ffffff";
-          ctx.textAlign = "center";
-          ctx.fillText(member.user.username, 545, 177);
-         
-          ctx.font = '16px Arial Bold';
-          ctx.fontSize = '72px';
-          ctx.fillStyle = "#ffffff";
-          ctx.textAlign = "center";
-          ctx.fillText(`${member.guild.memberCount} Members`, 580, 200);
-         
-          let Avatar = Canvas.Image;
-          let ava = new Avatar;
-          ava.src = buf;
-          ctx.beginPath();
-          ctx.arc(169.5, 148, 126.9, -100, Math.PI * 2, true);
-          ctx.closePath();
-          ctx.clip();
-          ctx.drawImage(ava, 36, 21, 260, 260);
-           
-          const c = client.channels.get("509766900117536791");
-          c.sendFile(canvas.toBuffer());
- 
-});
-});
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
 });
 
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("491686948130521112");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        });
+
+client.on("message", message => {
+    var prefix = "S";
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc2') {
+        if (!args[1]) {
+    message.channel.send("**bc2 <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                m.send(args);
+            });
+            const AziRo = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)   
+            .setTitle('✅| جاري ارسال رسالتك ') 
+            .addBlankField(true)
+            .addField('♨| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
+            .addField('📝| الرسالة ', args)
+            .setColor('RANDOM')  
+            message.channel.sendEmbed(AziRo);          
+        }
+        } else {
+            return;
+        }
     });
-});
-var dat = JSON.parse(fs.readFileSync('./invite.json', 'utf8'));
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("507144416721371143")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'stranger');
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("507144416721371143")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-channel.send(`**.#WeeeelcomeTo___Śtranger___,**:rose:`)
- channel.send(`***___JoinBy___***: ${Invite.inviter}`)
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
-});
-
-
-
-
-
-
-
-
-
-
-
 
 
 
