6ff8a4b2d1025fe5cf6f647b88a08c44936bfaa2
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-11-07 22:20:39 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,19 +82,7 @@ client.on('message', message => {
 
 
 
-client.on("message", message => {
-          var prefix = "S";
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
+
 
 
    
