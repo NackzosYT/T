5725ea45463a5712bdb02c26101b6f96954fa1fe
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-11-16 00:14:34 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -424,50 +424,6 @@ message.author.send(`
 
 
 
-client.on('message', message => {
-    const prefix = 'B'
-var args = message.content.split(" ").slice(1);    
-if(message.content.startsWith(prefix + 'id')) {
-var year = message.author.createdAt.getFullYear()
-var month = message.author.createdAt.getMonth()
-var day = message.author.createdAt.getDate()
-var men = message.mentions.users.first();  
-let args = message.content.split(' ').slice(1).join(' ');
-if (args == '') {
-var z = message.author;
-}else {
-var z = message.mentions.users.first();
-}
-
-let d = z.createdAt;          
-let n = d.toLocaleString();   
-let x;                       
-let y;                        
-
-if (z.presence.game !== null) {
-y = ${z.presence.game.name};
-} else {
-y = "No Playing... |??.";
-}
-let embed = new Discord.RichEmbed()
-.setColor("#502faf")
-.addField(': ?? | اسمك',`**<@` + ${z.id} + >**, true)
-.addField(': ?? | ايديك', "*"+ ${z.id} +"*",true)
-.addField(': ? | Playing','*'+y+'*' , true)
-.addField(': ?? | تاق حق حسابك',"**#" +  ${z.discriminator}**,true)
-.addField('*: ?? | التاريح الذي انشئ فيه حسابك*', message.author.createdAt.toLocaleString())
-.addField("*: ? | تاريخ دخولك للسيرفر*", message.member.joinedAt.toLocaleString())    
-
-.setThumbnail(${z.avatarURL})
-.setFooter(message.author.username, message.author.avatarURL)
-
-message.channel.send({embed});
-    if (!message) return message.reply('*ضع المينشان بشكل صحيح  ? *').catch(console.error);
-
-}
-
-});
-
 
 
 
