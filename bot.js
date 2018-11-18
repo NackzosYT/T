db8cf069983b9621240223f7125d885c50f5bad8
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-11-18 19:28:14 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,27 +38,65 @@ client.on('message', message => {
 
 
 
+//////////////////
 
+const sWlc = {}
+client.on('message', message => {
+if(message.channel.type === "dm") return;
+if(message.author.bot) return;
+  if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
+    channel: "blood"
+}
+const channel = sWlc[message.guild.id].channel
+  if (message.content.startsWith(prefix + "setwelcomer")) {
+    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+    let newChannel = message.content.split(' ').slice(1).join(" ")
+    if(!newChannel) return message.reply(`**${prefix}setwelcomer <channel name>**`)
+    sWlc[message.guild.id].channel = newChannel
+    message.channel.send(`**${message.guild.name}'s channel has been changed to ${newChannel}**`);
+  }
+});
+client.on("guildMemberAdd", member => {
+      if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
+    channel: "blood"
+  }
+  const channel = sWlc[member.guild.id].channel
+    const sChannel = sWlc[member.guild.id].channel
+    let welcomer = member.guild.channels.find('name', sChannel);
+    let memberavatar = member.user.avatarURL
+      if (!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = mecmber.user;
+        let heroo = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
+         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:heroo});
 
+      var Canvas = require('canvas')
+      var jimp = require('jimp')
 
-client.on('guildMemberAdd', member => {
-     const welcomer =  member.guild.channels.find('name', 'blood');
+      const w = ['w1.png'];
 
-const w = ['w1.png'];
-         let Image = Canvas.Image,
-            canvas = new Canvas(400, 239),
-            ctx = canvas.getContext('2d');
-        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-            if (err) return console.log(err);
-            let BG = Canvas.Image;
-            let ground = new Image;
-            ground.src = Background;
-            ctx.drawImage(ground, 0, 0, 400, 239);
-             
-          
+              let Image = Canvas.Image,
+                  canvas = new Canvas(400, 239),
+                  ctx = canvas.getContext('2d');
 
-                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(100) + ".png" : member.user.displayAvatarURL;
-                jimp.read(url, (err, ava) => {
+              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+                  if (err) return console.log(err)
+                  let BG = Canvas.Image;
+                  let ground = new Image;
+                  ground.src = Background;
+                  ctx.drawImage(ground, 0, 0, 400, 239);
+
+      })
+
+                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
+               jimp.read(url, (err, ava) => {
                     if (err) return console.log(err);
                     ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
                         if (err) return console.log(err);
@@ -74,16 +112,21 @@ const w = ['w1.png'];
                         ctx.fillStyle = "#f1f1f1";
                         ctx.textAlign = "center";
                         ctx.fillText(member.user.username, 240, 210);
+
+                              //AVATARً
  
                 let Avatar = Canvas.Image;
                               let ava = new Avatar;
                               ava.src = buf;
                               ctx.beginPath();
                               ctx.arc(84, 149, 72, 0, Math.PI*2);
-                              ctx.stroke();
+                   ctx.closePath();
+
                                  ctx.clip();
+
                                  ctx.drawImage(ava, 12, 77, 149, 149);
-                          
+                              ctx.closePath();
+                         
                 
                              
 welcomer.sendFile(canvas.toBuffer())
@@ -112,6 +155,7 @@ channel.send(`**.#WeeeelcomeTo___Blood___.** :wine_glass:`)
 
 
 });
+
 
 var dat = JSON.parse("{}");
 client.on("guildMemberAdd", (member) => {
@@ -142,14 +186,7 @@ client.on("guildMemberAdd", (member) => {
 });
 
 
-
-
-
-
-
-
-
-
+////////////////
 
 
 
@@ -500,7 +537,7 @@ function play(guild, song) {
 
 
 client.on('message', message => {
-if (message.content.split(' ')[0] == '-m')
+if (message.content.split(' ')[0] == 'Bbc')
  message.guild.members.forEach( member => {
          if (!message.member.hasPermission("CONNECT"))  return;
 member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
